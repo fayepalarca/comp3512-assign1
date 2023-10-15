e7fabec202f1220828c35dcc62467df701a861e1
Rebeka O <91212802+rebekano@users.noreply.github.com>
2023-10-15 01:55:54 -0600

    about us php uploaded onto includes folder

---
+++ includes/aboutus.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, inital-scale="1.0">
+        <link rel="stylesheet" href="homepage.css"/>
+        <title> </title>
+</head>
+
+<div class="header">
+    <h1> About Us and Our Project</h1> 
+</div>
+<body>
+    <p> COMP 3512 - Web Development II Assignment 1 <br>
+        In this assignment, we used HTML along with PHP to implement a table where songs could be populated by user request and interact with the databse to pull the information. We used CSS to style our php files as well as SQL queries to gather the data and prompt the information to the page. <br>
+        Rebeka Ochieng, Faye Palarca, Guranjan Singh <br>
+        Resources that we used for our CSS styling: 
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <https://www.w3schools.com/css/css_navbar_horizontal.asp>
+        <https://www.w3schools.com/howto/howto_js_list_grid_view.asp>
+        https://github.com/fayepalarca/comp3512-assign1.git</p><br>
+        https://github.com/rebekano<br>
+        https://github.com/singhmru868 <br>
+
+    </p>
+</body>
+
+<div class="footer">
+    <p> </p>
+</div>
+
+</html>
